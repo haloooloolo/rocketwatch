3810c3c16215897b3eb8d7236f999a02c1200884
haloooloolo <03_sharks.guises@icloud.com>
2026-04-09 11:27:36 +0000

    update obfuscation detection

--- rocketwatch/plugins/scam_detection/checks.py
+++ rocketwatch/plugins/scam_detection/checks.py
@@ -30,7 +30,8 @@ class ScamChecks:
             rf"<{_ws}ht{_brk}tp|"  # <ht\n> tp
             rf"<{_ws}ma{_ws}i{_brk}l{_ws}t{_ws}o|"  # <ma\n> i\n> L\n> To (mailto)
             rf"<d{_brk}?i{_brk}?s{_brk}?c{_brk}?o{_brk}?r|"  # <discord: broken across lines
-            rf"di{_brk}sco{_brk}rd(?!\.(?:com|gg|py|js|net|org))",  # di\nsco\nrd (not discord.com etc)
+            rf"di{_brk}sco{_brk}rd(?!\.(?:com|gg|py|js|net|org))|"  # di\nsco\nrd (not discord.com etc)
+            rf"dis{_brk}cord",  # dis\ncord (alternate break position)
             re.IGNORECASE,
         )
         # Detects fullwidth/homoglyph dots used to disguise domains
